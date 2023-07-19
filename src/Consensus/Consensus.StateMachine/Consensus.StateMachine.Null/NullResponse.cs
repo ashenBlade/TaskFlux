de2fd0d2ea2c6b3fefd1189ed67742e06e8a75dd
ash.blade <ashen.blade.dev@gmail.com>
2023-07-19 20:36:23 +0300

    refactor: удалил проект с NullStateMachine

--- src/Consensus/Consensus.StateMachine/Consensus.StateMachine.Null/NullResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Consensus.StateMachine.Null;
-
-public class NullResponse
-{
-    public static readonly NullResponse Instance = new();
-    
-    public void WriteTo(Stream output)
-    { }
-}
